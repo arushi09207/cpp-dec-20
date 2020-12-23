6b8b66ad2df868438ec346e23754e16cf0a61718
arushi09207 <63094403+arushi09207@users.noreply.github.com>
2020-12-23 21:11:49 +0530

    Create task-1.cpp

---
+++ task-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 9;
+    float b = 8.9;
+    double c = 1.000003004040030999098732;
+    char d = 'A';
+    cout << sizeof(a) << "\n";
+    cout << sizeof(b) << "\n";
+    cout << sizeof(c) << "\n";
+    cout << sizeof(d) << "\n";
+
+    return 0;
+}
